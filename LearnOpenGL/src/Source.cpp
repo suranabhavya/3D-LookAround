2110bdf495e64444a725e7e647f05e453822782f
Bhavya Surana <bhavyasurana2000@gmail.com>
2020-06-24 14:59:37 +0530

    Updated the code to make it look like a Road. Texture Co-ordinates are not correct tho!

--- LearnOpenGL/src/Source.cpp
+++ LearnOpenGL/src/Source.cpp
@@ -59,7 +59,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Road", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -93,60 +93,61 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        // Cuboid Coord       // Texture coord
+         0.7f,  0.1f, -0.5f,  0.0f, 0.0f,
+         0.7f,  0.1f,  0.5f,  1.0f, 0.0f,
+        -0.7f,  0.1f, -0.5f,  1.0f, 1.0f,
+        -0.7f,  0.1f,  0.5f,  1.0f, 1.0f,
+        -0.7f,  0.1f, -0.5f,  0.0f, 1.0f,
+         0.7f,  0.1f,  0.5f,  0.0f, 0.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.7f, -0.1f, -0.5f,  0.0f, 0.0f,
+         0.7f, -0.1f,  0.5f,  1.0f, 0.0f,
+        -0.7f, -0.1f, -0.5f,  1.0f, 1.0f,
+        -0.7f, -0.1f,  0.5f,  1.0f, 1.0f,
+        -0.7f, -0.1f, -0.5f,  0.0f, 1.0f,
+         0.7f, -0.1f,  0.5f,  0.0f, 0.0f,
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.7f,  0.1f,  0.5f,  1.0f, 0.0f,
+         0.7f, -0.1f,  0.5f,  1.0f, 1.0f,
+        -0.7f,  0.1f,  0.5f,  0.0f, 1.0f,
+        -0.7f, -0.1f,  0.5f,  0.0f, 1.0f,
+        -0.7f,  0.1f,  0.5f,  0.0f, 0.0f,
+         0.7f, -0.1f,  0.5f,  1.0f, 0.0f,
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.7f,  0.1f, -0.5f,  1.0f, 0.0f,
+         0.7f, -0.1f, -0.5f,  1.0f, 1.0f,
+        -0.7f,  0.1f, -0.5f,  0.0f, 1.0f,
+        -0.7f, -0.1f, -0.5f,  0.0f, 1.0f,
+        -0.7f,  0.1f, -0.5f,  0.0f, 0.0f,
+         0.7f, -0.1f, -0.5f,  1.0f, 0.0f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.7f,  0.1f, -0.5f,  0.0f, 1.0f,
+         0.7f,  0.1f,  0.5f,  1.0f, 1.0f,
+         0.7f, -0.1f, -0.5f,  1.0f, 0.0f,
+         0.7f, -0.1f,  0.5f,  1.0f, 0.0f,
+         0.7f,  0.1f,  0.5f,  0.0f, 0.0f,
+         0.7f, -0.1f, -0.5f,  0.0f, 1.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+        -0.7f,  0.1f, -0.5f,  0.0f, 1.0f,
+        -0.7f,  0.1f,  0.5f,  1.0f, 1.0f,
+        -0.7f, -0.1f, -0.5f,  1.0f, 0.0f,
+        -0.7f, -0.1f,  0.5f,  1.0f, 0.0f,
+        -0.7f,  0.1f,  0.5f,  0.0f, 0.0f,
+        -0.7f, -0.1f, -0.5f,  0.0f, 1.0f
     };
     // world space positions of our cubes
-    glm::vec3 cubePositions[] = {
+    glm::vec3 cuboidPositions[] = {
         glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+        glm::vec3(0.0f,  0.0f, -1.0f),
+        glm::vec3(0.0f,  0.0f, -2.0f),
+        glm::vec3(0.0f,  0.0f, -3.0f),
+        glm::vec3(0.0f,  0.0f, -4.0f),
+        glm::vec3(0.0f,  0.0f, -5.0f),
+        glm::vec3(0.0f,  0.0f, -6.0f),
+        glm::vec3(0.0f,  0.0f, -7.0f),
+        glm::vec3(0.0f,  0.0f, -8.0f),
+        glm::vec3(0.0f,  0.0f, -9.0f)
     };
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -181,7 +182,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char* data = stbi_load("textures/stone.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("textures/road.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -239,7 +240,7 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // bind textures on corresponding texture units
@@ -265,9 +266,9 @@ int main()
         {
             // calculate the model matrix for each object and pass it to shader before drawing
             glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-            model = glm::translate(model, cubePositions[i]);
+            model = glm::translate(model, cuboidPositions[i]);
             float angle = 50.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            // model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
